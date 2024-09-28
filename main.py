71b529c7355d3e19247d33db8b7d22007cc80054
Alexandr Andreev <aleksandrandreev@MacBook-Pro-Aleksandr.local>
2024-09-28 20:32:50 +0300

    Change errors writer

--- main.py
+++ main.py
@@ -13,16 +13,16 @@ def main():
     init_csv_result_file()
 
     # запись в результирующий файл, возвращение ошибок
-    errors_count, error_rows_numbers = write_result_file(config["filename_from_1c"])
+    skip_rows_count, errors = write_result_file(config["filename_from_1c"])
 
     # запись ошибок в errors.txt
-    write_error_rows(errors_count, error_rows_numbers)
+    write_error_rows(skip_rows_count, errors)
 
 
-def write_result_file(filename: str) -> (int, List[int]):
+def write_result_file(filename: str) -> (int, list):
     """Получение данных из файла выгрузки из 1С, запись в результирующий файл, возвращение ошибок"""
     skip_row_count = 0
-    skipped_rows = []
+    errors = []
 
     with open(filename, newline="\n", encoding="cp1251") as file:
         reader = csv.reader(file.read().splitlines(), delimiter=';')
@@ -35,7 +35,7 @@ def write_result_file(filename: str) -> (int, List[int]):
                 continue
 
             # debug version
-            if count > 10:
+            if count > 1000:
                 break
 
             print(f"Обрабатывается строка № {count}")
@@ -44,22 +44,26 @@ def write_result_file(filename: str) -> (int, List[int]):
             if len(row) < 34 or row[0] != "Товары":
                 count += 1
                 skip_row_count += 1
-                skipped_rows.append(count)
+                errors.append([f"{count}"] + ["Строка некорректная"])
                 continue
-
-            # получаем готовую строку для записи
-            result_row = get_result_row(row)
-
-            write_to_csv_file(result_row)
 
             count += 1
 
-    return skip_row_count, skipped_rows
+            # получаем готовую строку для записи
+            result_row, row_errors = get_result_row(row)
+
+            if row_errors:
+                errors.append([f"{count}"] + row_errors)
+
+            write_to_csv_file(result_row)
+
+    return skip_row_count, errors
 
 
-def get_result_row(csv_row: list) -> list:
+def get_result_row(csv_row: list) -> (list, list):
     """Формирование финальной строки для авито из строки csv файла"""
     result_row = []
+    row_errors = []
 
     result_row.append(csv_row[4])   # Id
     result_row.append("")   # AvitoId
@@ -84,7 +88,10 @@ def get_result_row(csv_row: list) -> list:
         result_row.append(config["goods_type"])  # GoodsType
         result_row.append(config["ad_type"])  # AdType
 
-    product_info = get_product_types(csv_row[12], csv_row[13])
+    product_info, error = get_product_types(csv_row[12], csv_row[13])
+    if error:
+        row_errors.append(error)
+
     result_row.append(product_info[0])  # ProductType
     result_row.append(product_info[1])  # SparePartType
     result_row.append(product_info[2])  # EngineSparePartType
@@ -109,7 +116,10 @@ def get_result_row(csv_row: list) -> list:
 
     # result_row.append("ПУСТАЯ СТРОКА")  # S пустая строка TODO убрать
 
-    make_model_generation = get_make_model_generation(csv_row[15], csv_row[16], csv_row[17])
+    make_model_generation, error = get_make_model_generation(csv_row[15], csv_row[16], csv_row[17])
+    if error:
+        row_errors.append(error)
+
     result_row.append(make_model_generation[0])  # Make строка
     result_row.append(make_model_generation[1])  # Model строка
     result_row.append(make_model_generation[2])  # Generation строка
@@ -127,10 +137,10 @@ def get_result_row(csv_row: list) -> list:
 
     result_row.append(csv_row[14])   # Price
 
-    return result_row
+    return result_row, row_errors
 
 
-def get_product_types(group_M: str, sub_group_N: str) -> list:
+def get_product_types(group_M: str, sub_group_N: str) -> (list, str):
     """Сопоставляем М (12), N (13) с G в таблице соответствия и полями B(1), C(2), D(3) заполняет поля J, K, L"""
     result = []
     for row in PRODUCT_TYPES:
@@ -141,9 +151,12 @@ def get_product_types(group_M: str, sub_group_N: str) -> list:
             result.append(row[3])
             result.append(row[4])
 
+            return result, ""
+
     if not result:
-        return ["ЗАГЛУШКА 1", "ЗАГЛУШКА 2", "ЗАГЛУШКА 3", "ЗАГЛУШКА 4", "ЗАГЛУШКА 5"]
-    return result
+        return ["ЗАГЛУШКА 1", "ЗАГЛУШКА 2", "ЗАГЛУШКА 3", "ЗАГЛУШКА 4", "ЗАГЛУШКА 5"], \
+            f"Не удалось найти соответствие ProductType, SparePartType, EngineSparePartType, BodySparePartType, " \
+            f"DeviceType по группе '{group_M}' и подгруппе '{sub_group_N}'"
 
 
 # TODO разобраться с пробелами
@@ -163,14 +176,17 @@ def get_oem_field(row: list) -> str:
     return oem
 
 
-def get_make_model_generation(make: str, model: str, generation: str) -> list:
+def get_make_model_generation(make: str, model: str, modification: str) -> (list, str):
     """Получение Make Model Generation Modification FuelType DriveType Transmission
     BodyType Doors по P Q R из 1С файла"""
     for row in MAKES_MODELS_GENERATIONS:
-        if row[10] == make and row[11] == model and row[12] == generation:
-            return row
+        if row[10] == make and row[11] == model and row[12] == modification:
+            return row, ""
 
-    return ["ЗАГЛУШКА 1", "ЗАГЛУШКА 2", "ЗАГЛУШКА 3", "ЗАГЛУШКА 4", "ЗАГЛУШКА 5", "ЗАГЛУШКА 6", "ЗАГЛУШКА 7", "ЗАГЛУШКА 8", "ЗАГЛУШКА 9"]
+    return ["ЗАГЛУШКА 1", "ЗАГЛУШКА 2", "ЗАГЛУШКА 3", "ЗАГЛУШКА 4", "ЗАГЛУШКА 5", "ЗАГЛУШКА 6", "ЗАГЛУШКА 7",
+            "ЗАГЛУШКА 8", "ЗАГЛУШКА 9"], \
+        f"Не удалось найти соответствие Make, Model, Generation, Modification, FuelType, DriveType, " \
+        f"Transmission, BodyType, Doors по марке '{make}', модели '{model}' и модификации '{modification}'"
 
 
 def write_to_csv_file(row: list):
@@ -191,16 +207,21 @@ def init_csv_result_file():
         writer.writerow(header)
 
 
-def write_error_rows(skip_row_count: int, skipped_rows: List[int]):
+def write_error_rows(skip_rows_count: int, errors: list):
     """Записывает необработанные строки из файла выгрузки 1С"""
     with open("errors.txt", "w") as file:
-        file.write("Список ошибок:\n\n")
+        file.write("СПИСОК ОШИБОК\n\n")
 
-    if skip_row_count > 0:
-        text = f"Пропущено строк: {skip_row_count}\n\nНомера пропущенных строк в файле выгрузки из 1С:\n{' '.join([f'{row_number};' for row_number in skipped_rows])}"
+    if len(errors) > 0:
+        text = f"Пропущено строк: {skip_rows_count}, всего ошибок: {len(errors)}\n\n" \
+               f"Ошибки в строках по номерам в файле выгрузки из 1С:\n\n"
+
+        for error in errors:
+            text += f"{error[0]}. {' | '.join(error[1:])}\n"
+
         with open("errors.txt", "a") as file:
             file.write(text)
-        print(f"Пропущено строк: {skip_row_count}. Их номера записаны в файле errors.txt")
+        print(f"Пропущено строк: {skip_rows_count}, всего ошибок {len(errors)}, они записаны в файле errors.txt")
     else:
         with open("errors.txt", "a") as file:
             file.write("Все строки записаны, ошибок нет")
