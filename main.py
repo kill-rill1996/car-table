61f2b48465b999bc715d8e737e423e5446bba00b
Kirill Andreev <hizenberg228@mail.ru>
2024-09-28 22:35:02 +0300

    Change print work time

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ from avito_table import AvitoTable
 
 if __name__ == "__main__":
     start = datetime.now()
+
     avito_table = AvitoTable()
     avito_table.make_avito_table()
+
     end = datetime.now()
-    print(end - start)
+    work_time = end - start
+    print(f"\nВремя выполнения программы {work_time}")
